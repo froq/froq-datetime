75e67bb66c86618576d0c7fa6f91b05a98fede67
Kerem Güneş <qeremy@gmail.com>
2022-06-19 00:08:57 +0300

    Add LocaleInfo class. * Locale:     - add prop $info [as LocaleInfo].     - add __construct() & getLanguage(),getCountry(),getEncoding().     - add make(),makeInfo() & parse().

--- src/Locale.php
+++ src/Locale.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
 namespace froq\date;
 
 /**
- * A class for (default) locale only.
+ * A class for locale time stuff.
  *
  * @package froq\date
  * @object  froq\date\Locale
@@ -20,8 +20,100 @@ class Locale
     /** @const int */
     public const DEFAULT = 'en_US.UTF-8';
 
+    /** Parsed locale info. */
+    public readonly LocaleInfo $info;
+
     /**
-     * Get LC_TIME value or default.
+     * Constructor.
+     *
+     * @param string $info
+     */
+    public function __construct(string $info)
+    {
+        $this->info = self::makeInfo($info);
+    }
+
+    /**
+     * Get language.
+     *
+     * @return string
+     */
+    public function getLanguage(): string
+    {
+        return $this->info->language;
+    }
+
+    /**
+     * Get country.
+     *
+     * @return string
+     */
+    public function getCountry(): string
+    {
+        return $this->info->country;
+    }
+
+    /**
+     * Get encoding.
+     *
+     * @return string|null
+     */
+    public function getEncoding(): string|null
+    {
+        return $this->info->encoding;
+    }
+
+    /**
+     * Make a locale using language, country and encoding params.
+     *
+     * @param  string      $language
+     * @param  string      $country
+     * @param  string|null $encoding
+     * @return froq\date\Locale
+     */
+    public static function make(string $language, string $country, string $encoding = null): Locale
+    {
+        $info = $language . '_' . $country;
+
+        if ($encoding != '') {
+            $info .= '.' . $encoding;
+        }
+
+        return new Locale($info);
+    }
+
+    /**
+     * Make a locale info using parsed language, country and encoding parts.
+     *
+     * @param  string $info
+     * @return froq\date\LocaleInfo
+     */
+    public static function makeInfo(string $info): LocaleInfo
+    {
+        $info = self::parse($info);
+
+        return new LocaleInfo(...$info);
+    }
+
+    /**
+     * Parse locale info to language, country and encoding parts.
+     *
+     * @param  string $info
+     * @return froq\date\LocaleInfo
+     */
+    public static function parse(string $info): array
+    {
+        $ret = ['language' => '', 'country' => '', 'encoding' => null];
+
+        if (preg_match('~([a-z]{2})(?:_([a-z-A-Z]{2}))?(?:\.([a-z-A-Z\d\-]+))?~', $info, $match)) {
+            $ret = array_combine(array_keys($ret), array_pad(array_slice($match, 1), 3, null));
+        }
+
+        return $ret;
+    }
+
+    /**
+     * Get LC_TIME info or default.
      *
      * @param  string|null $default
      * @return string
---
+++ src/LocaleInfo.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Copyright (c) 2015 · Kerem Güneş
+ * Apache License 2.0 · http://github.com/froq/froq-date
+ */
+declare(strict_types=1);
+
+namespace froq\date;
+
+/**
+ * A read-only locale info entry class.
+ *
+ * @package froq\date
+ * @object  froq\date\LocaleInfo
+ * @author  Kerem Güneş
+ * @since   6.0
+ * @internal
+ */
+final class LocaleInfo
+{
+    /**
+     * Constructor.
+     */
+    public function __construct(
+        public readonly string  $language,
+        public readonly string  $country,
+        public readonly ?string $encoding = null,
+    )
+    {}
+}
