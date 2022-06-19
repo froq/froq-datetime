9a0790af4514c5ab3367ce7b34dd02245192ed78
Kerem Güneş <qeremy@gmail.com>
2022-06-19 19:29:42 +0300

    Add UnixTimeException. * UnixTime:     - make constructable [drop static state].     - add prop $time.     - add setTime(),getTime().     - add make().     - change from() => convert(), optimize.

--- src/UnixTime.php
+++ src/UnixTime.php
@@ -10,16 +10,57 @@ namespace froq\date;
 use DateTime;
 
 /**
- * Just for only syntactic sugar delight.
+ * Unix time utility class.
  *
  * @package froq\date
  * @object  froq\date\UnixTime
  * @author  Kerem Güneş
- * @since   4.0, 5.0
- * @static
+ * @since   4.0, 5.0, 6.0
  */
 class UnixTime
 {
+    /** @var int */
+    protected int $time;
+
+    /**
+     * Constructor.
+     *
+     * @param  int|string|froq\date\Date|DateTime|null $when
+     * @throws froq\date\UnixTimeException
+     */
+    public function __construct(int|string|Date|DateTime $when = null)
+    {
+        if ($when !== null) {
+            if (is_int($when)) {
+                $this->time = $when;
+            } else {
+                $this->time = self::convert($when) ??
+                    throw new UnixTimeException('Invalid time: ' . $when);
+            }
+        }
+    }
+
+    /**
+     * Set time.
+     *
+     * @param  int $time
+     * @return void
+     */
+    public function setTime(int $time): void
+    {
+        $this->time = $time;
+    }
+
+    /**
+     * Get time.
+     *
+     * @return int|null
+     */
+    public function getTime(): int|null
+    {
+        return $this->time ?? null;
+    }
+
     /**
      * Now.
      *
@@ -31,14 +72,38 @@ class UnixTime
     }
 
     /**
-     * Convert given date to unixtime.
+     * Make a Unix time.
      *
-     * @param  string|Date|DateTime $when
-     * @return int
+     * @param  int      $hour,
+     * @param  int|null $minute
+     * @param  int|null $second
+     * @param  int|null $month
+     * @param  int|null $day
+     * @param  int|null $year
+     * @return int|null
      */
-    public static function from(string|Date|DateTime $when): int
+    public static function make(
+        int $year, int $month = null, int $day = null,
+        int $hour = null, int $minute = null, int $second = null,
+    ): int|null
     {
-        return is_string($when) ? strtotime($when) : $when->getTimestamp();
+        $time =@ mktime($hour ?? 0, $minute, $second, $month, $day, $year);
+        return ($time !== false) ? $time : null;
+    }
+
+    /**
+     * Convert given date to Unix time.
+     *
+     * @param  string|froq\date\Date|DateTime $when
+     * @return int|null
+     */
+    public static function convert(string|Date|DateTime $when): int|null
+    {
+        if (is_string($when)) {
+            $time =@ strtotime($when);
+            return ($time !== false) ? $time : null;
+        }
+        return $when->getTimestamp();
     }
 }
 
